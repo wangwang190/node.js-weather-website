fce898da19af91cfc8163ccb80736a391274f7dc
wang190 <89770062+wangwang190@users.noreply.github.com>
2021-10-21 22:14:16 +0800

    uodated Hereku port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 const path = require("path")
 const express = require("express")
 const app = express()
-const port = process.argv.PORT || 3000
+const port = process.env.PORT || 3000
 const hbs = require("hbs")
 const { registerPartial } = require("hbs")
 const geoCode = require("./utils/geocode.js")
